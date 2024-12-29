2e7ec433298c1da525d52741a413eeb45536b167
Hugo Fr <51454951+Ryuggo@users.noreply.github.com>
2024-12-29 17:05:40 +0100

    Added ui
    input_manager launches a signal for every actions linked to a button depending on the press type

--- input_manager.gd
+++ input_manager.gd
@@ -2,9 +2,11 @@ extends Node
 
 @export var debug: bool = false
 @export var minimum_hold_timer = 0.2
+@export var ui_start_with: String = "ui_"
+@export var allow_multiple_actions_per_key: bool = true
 
-var inputs_gui: Dictionary = {}
-var inputs_game: Dictionary = {}
+var inputs: Dictionary = {}
+var current_inputs: Dictionary
 
 enum InputPressTypes{
 	TAP = 1,			# Triggers only when a button is pushed down and released quickly
@@ -16,22 +18,22 @@ enum InputPressTypes{
 	RELEASE = 7			# Triggers when the button is released after being pressed down
 }
 
-var current_inputs: Dictionary
-var pressed_inputs: Array[String]
-
 signal gui_input(action: String)
 signal gameplay_input(action: String)
 
 
 func _ready() -> void:
-	if FileAccess.file_exists("user://keybindings.json"):
+	if !FileAccess.file_exists("user://keybindings.json"):
+		create_inputs()
+		save_inputs()
+	else:
 		load_inputs()
-		return
-	
+
+
+func create_inputs() -> void:
 	for actionName in InputMap.get_actions():
 		for inputEvent in InputMap.action_get_events(actionName):
 			var key: String
-			var action: Dictionary
 			if inputEvent is InputEventKey:
 				key = OS.get_keycode_string(
 					DisplayServer.keyboard_get_label_from_physical(
@@ -46,14 +48,13 @@ func _ready() -> void:
 					)
 			else:
 				key = inputEvent.as_text()
-			action[InputPressTypes.PRESS] = actionName
-			if actionName.begins_with("ui_"):
-				inputs_gui[key] = actionName
+			
+			if !inputs.has(key):
+				var action: Dictionary
+				action[str(InputPressTypes.PRESS)] = [actionName]
+				inputs[key] = action
 			else:
-				inputs_game[key] = action
-	
-	save_inputs()
-
+				inputs[key][str(InputPressTypes.PRESS)].append(actionName)
 
 func load_inputs() -> void:
 	var file = FileAccess.open("user://keybindings.json", FileAccess.READ)
@@ -64,41 +65,21 @@ func load_inputs() -> void:
 	if json.parse(json_string) == OK:
 		var data_received = json.data
 		for key in data_received:
-			if typeof(data_received[key]) == TYPE_DICTIONARY:
-				inputs_game[key] = data_received[key]
-			else:
-				inputs_gui[key] = data_received[key]
+			inputs[key] = data_received[key]
 	else:
 		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
 
 func save_inputs() -> void:
-	var json_string = (JSON.stringify(inputs_gui)).left(-1)
-	json_string += "," + (JSON.stringify(inputs_game).right(-1))
+	var json_string = (JSON.stringify(inputs, "\t"))
 	var file = FileAccess.open("user://keybindings.json", FileAccess.WRITE)
 	file.store_string(json_string)
 	file.close()
 
 
-# Better for GUI
 func _input(event):
-	if !inputs_gui.has(event.as_text()): return
-	
-	gui_input.emit(inputs_gui[event.as_text()])
-	#get_tree().get_root().set_input_as_handled()
-
-# Better for idk
-func _shortcut_input(event: InputEvent) -> void:
-	#if debug : print("shortcut")
-	#input_manager(event)
-	#get_tree().get_root().set_input_as_handled()
-	pass
-
-# Better for Gameplay
-func _unhandled_input(event: InputEvent) -> void:
-	if !inputs_game.has(event.as_text()): return
+	if !inputs.has(event.as_text()): return
 	
 	input_manager(event)
-	get_tree().get_root().set_input_as_handled()
 
 
 func input_manager(event: InputEvent) -> void:
@@ -106,14 +87,14 @@ func input_manager(event: InputEvent) -> void:
 	
 	if event.is_pressed():
 		if !current_inputs.has(action_name):
-			send_gameplay_signal(event.as_text(), InputPressTypes.PRESS)
+			send_signal(event.as_text(), InputPressTypes.PRESS)
 			current_inputs[action_name] = InputPressTypes.PRESS
 		elif current_inputs[action_name] == InputPressTypes.RELEASE:
-			send_gameplay_signal(event.as_text(), InputPressTypes.DOUBLE_TAP)
+			send_signal(event.as_text(), InputPressTypes.DOUBLE_TAP)
 		
-		send_gameplay_signal(event.as_text(), InputPressTypes.HOLD)
+		send_signal(event.as_text(), InputPressTypes.HOLD)
 		if current_inputs[action_name] == InputPressTypes.LONG_PRESS:
-			send_gameplay_signal(event.as_text(), InputPressTypes.LONG_HOLD)
+			send_signal(event.as_text(), InputPressTypes.LONG_HOLD)
 		
 		await get_tree().create_timer(minimum_hold_timer).timeout
 		if !current_inputs.has(action_name): return
@@ -121,26 +102,28 @@ func input_manager(event: InputEvent) -> void:
 		if current_inputs[action_name] != InputPressTypes.PRESS: return
 		
 		current_inputs[action_name] = InputPressTypes.LONG_PRESS
-		send_gameplay_signal(event.as_text(), InputPressTypes.LONG_PRESS)
+		send_signal(event.as_text(), InputPressTypes.LONG_PRESS)
 	elif event.is_released():
-		send_gameplay_signal(event.as_text(), InputPressTypes.RELEASE)
+		send_signal(event.as_text(), InputPressTypes.RELEASE)
 		
 		if !current_inputs.has(action_name): return
 		
 		if current_inputs[action_name] == InputPressTypes.PRESS:
-			send_gameplay_signal(event.as_text(), InputPressTypes.TAP)
+			send_signal(event.as_text(), InputPressTypes.TAP)
 		
 		current_inputs[action_name] = InputPressTypes.RELEASE
 		
 		await get_tree().create_timer(minimum_hold_timer).timeout
 		current_inputs.erase(action_name)
 
-
-func send_gameplay_signal(event: String, press_type: InputPressTypes) -> void:
-	if debug: print(event + " - " + str(press_type))
+func send_signal(event: String, press_type: InputPressTypes) -> void:
 	var press = str(press_type)
-	if !inputs_game[event].has(press): return
-	gameplay_input.emit(inputs_game[event][press])
+	if !inputs[event].has(press): return
+	for action in inputs[event][press]:
+		if action.begins_with(ui_start_with):
+			gui_input.emit(action)
+		else:
+			gameplay_input.emit(action)
 
 
 func _on_gameplay_input(action: String) -> void:
@@ -148,3 +131,16 @@ func _on_gameplay_input(action: String) -> void:
 
 func _on_gui_input(action: String) -> void:
 	if debug: print("Gui Signal : " + action)
+
+
+func _on_reset_pressed() -> void:
+	inputs.clear()
+	create_inputs()
+	save_inputs()
+
+func _on_save_pressed() -> void:
+	if allow_multiple_actions_per_key:
+		save_inputs()
+		return
+	
+	#TODO Add logic
